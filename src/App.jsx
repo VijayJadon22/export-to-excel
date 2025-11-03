9460245a62871a611ee68b4bb3033c34d61cc156
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-11-03 23:48:50 +0530

    Used the ExportToExcel Component to export the data as excel and it is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import ExportToExcel from "./components/ExportToExcel.jsx";
 
 export default function App() {
-  return <div>App</div>;
+  const users = [
+    { id: 1, name: "Alice", email: "alice@example.com", amount: 1200 },
+    { id: 2, name: "Bob", email: "bob@example.com", amount: 900 },
+    { id: 3, name: "Charlie", email: "charlie@example.com", amount: 1500 },
+  ];
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Amount</th>
+        </tr>
+        {users?.map((user, idx) => (
+          <tr key={idx}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.amount}</td>
+          </tr>
+        ))}
+      </table>
+      <ExportToExcel data={users} />
+    </div>
+  );
 }
